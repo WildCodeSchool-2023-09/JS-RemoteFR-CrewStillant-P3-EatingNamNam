4a948eb2a17a8e15206a4f73fcceb2afe29c645f
Mewlyscin <Tear-Lyons7@live.fr>
2023-12-20 11:49:33 +0100

    fix color navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,19 +4,19 @@ import logo from "../assets/logo_et_icones/logo1.png";
 
 function Navbar() {
   return (
-    <div className="bg-orangep flex flex-row justify-between">
+    <div className="bg-orange flex flex-row justify-between">
       <NavLink className="m-2" to="/">
         <img className="h-32 rounded-full" src={logo} alt="logo" />
       </NavLink>
-      <div className="text-beigep m-4 flex gap-2">
+      <div className="text-beige m-4 flex gap-2">
         <NavLink
-          className="hover:text-greenp text-xl h-fit my-auto"
+          className="hover:text-green text-xl h-fit my-auto"
           to="/recettes"
         >
           Mes recettes
         </NavLink>
         <NavLink
-          className="hover:text-greenp text-xl h-fit my-auto"
+          className="hover:text-green text-xl h-fit my-auto"
           to="/compte"
         >
           Mon compte
