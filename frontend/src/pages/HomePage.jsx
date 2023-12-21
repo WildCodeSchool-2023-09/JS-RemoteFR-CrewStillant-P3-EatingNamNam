cef55a11fbf604ec20a4a9bf83d8ac06dc2570e3
BAYRAM TUGBA <tugbabayram69@gmail.com>
2023-12-21 16:07:20 +0100

    fix: change is done

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,7 +4,7 @@ function HomePage() {
   const recipes = useLoaderData();
 
   return (
-    <div className="m-24">
+    <div className="m-20">
       <div>
         <ul className="flex flex-row justify-center flex-wrap gap-16">
           <div className="rounded-2xl w-72 h-80 p-4 bg-green text-center">
@@ -13,7 +13,7 @@ function HomePage() {
               <div className="flex flex-col items-center justify-center mt-16">
                 <button
                   type="button"
-                  className="rounded-full bg-beige p-10 text-green font-bold text-5xl"
+                  className="rounded-full bg-beige p-10 text-green font-bold text-4xl"
                 >
                   +
                 </button>
