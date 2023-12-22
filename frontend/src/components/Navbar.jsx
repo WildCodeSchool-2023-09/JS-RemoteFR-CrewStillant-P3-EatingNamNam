f502d975b641c237073755c5262ea77107a2a021
BAYRAM TUGBA <tugbabayram69@gmail.com>
2023-12-22 12:43:09 +0100

    fix: conflits

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
       <NavLink className="m-2" to="/">
         <img className="h-32 rounded-full" src={logo} alt="logo" />
       </NavLink>
-      <div className="text-beige m-4 flex gap-2">
+      <div className="text-beige m-4 flex gap-3">
         <NavLink className="hover:text-green text-xl h-fit my-auto" to="/">
           Mes recettes
         </NavLink>
