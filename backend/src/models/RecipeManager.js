db1a85658a8afae4410119b1a82142a5c58639ef
BAYRAM TUGBA <tugbabayram69@gmail.com>
2023-12-23 00:27:26 +0100

    fix: conflits

--- backend/src/models/RecipeManager.js
+++ backend/src/models/RecipeManager.js
@@ -31,7 +31,19 @@ class RecipeManager extends AbstractManager {
 
   async read(id) {
     const [rows] = await this.database.query(
-      `SELECT * FROM ${this.table} WHERE id=?`,
+      `SELECT
+        recipe.id,
+        recipe.title,
+        recipe.cooking_time,
+        recipe.preparation_time,
+        recipe.difficulty,
+        recipe.image,
+        ingredient_recipe.quantity,
+        ingredient.name 
+      FROM ${this.table}
+      JOIN ingredient_recipe ON recipe.id = ingredient_recipe.recipe_id
+      JOIN ingredient ON ingredient_recipe.ingredient_id = ingredient.id
+      WHERE recipe.id = ?`,
       [id]
     );
     return rows[0];
--- frontend/src/components/Recipes.jsx
+++ frontend/src/components/Recipes.jsx
@@ -3,12 +3,41 @@ import PropTypes from "prop-types";
 
 function Recipes({ recipe }) {
   return (
-    <div>
-      <div>
-        <img src={recipe.image} alt={recipe.image} />
-        <p>Préparation: {recipe.preparation_time}</p>
-        <p>Cuisson: {recipe.cooking_time}</p>
-        <p>Difficulté: {recipe.difficulty}</p>
+    <div className="m-3 text-xl">
+      <img
+        src={recipe.image}
+        alt={recipe.image}
+        className="mb-3 w-68 rounded-3xl"
+      />
+      <div className="border rounded-2xl flex flex-row justify-around mb-3 p-3 text-beige bg-orange">
+        <div className="text-center">
+          <p>Préparation: </p>
+          <div>{recipe.preparation_time}min</div>
+        </div>
+        <div className="text-center">
+          <p>Cuisson: </p>
+          <div className="flex justify-around">{recipe.cooking_time}min</div>
+        </div>
+        <div className="text-center">
+          <p>Difficulté:</p>
+          <div>{recipe.difficulty}</div>
+        </div>
+      </div>
+      <div className="border rounded-2xl mb-4 p-4 text-beige bg-orange">
+        <p>Liste des ingrédients:</p>
+        <ul>
+          <li className="flex flex-row justify-between">
+            {recipe.name}: <div>{recipe.quantity}g</div>
+          </li>
+        </ul>
+      </div>
+      <p className="text-orange mb-3">Etapes de préparation:</p>
+      <div className="border-green border-4 rounded-2xl p-3 bg-slate-200 mb-4">
+        {recipe.name}
+      </div>
+      <p className="text-orange  mb-3">Commentaires:</p>
+      <div className="border-green border-4 rounded-2xl p-3 bg-slate-200">
+        commentaires
       </div>
     </div>
   );
@@ -17,9 +46,11 @@ function Recipes({ recipe }) {
 Recipes.propTypes = {
   recipe: PropTypes.shape({
     image: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
     cooking_time: PropTypes.number.isRequired,
     preparation_time: PropTypes.number.isRequired,
     difficulty: PropTypes.number.isRequired,
+    quantity: PropTypes.number.isRequired,
   }).isRequired,
 };
 
