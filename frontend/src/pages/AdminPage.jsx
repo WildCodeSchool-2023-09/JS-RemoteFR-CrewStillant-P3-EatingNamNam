514fe97b10184f8e7293fa9f16bd04a5438e3ec2
Pierre Arnaudet <pierre.arnaudet44@gmail.com>
2024-01-11 10:59:32 +0100

    final touch thx to Ayoub

--- frontend/src/pages/AdminPage.jsx
+++ frontend/src/pages/AdminPage.jsx
@@ -1,18 +1,13 @@
+import { saveAs } from "file-saver";
+
 export default function AdminPage() {
   const handleClick = async () => {
     try {
       const csv = await fetch(
         `${import.meta.env.VITE_BACKEND_URL}/api/user/dl`
       ).then((res) => res.blob());
-      const link = document.createElement("a");
-      link.href = URL.createObjectURL(csv);
-      link.download = "users_informations.csv";
-      link.click();
-      // const url = URL.createObjectURL(csv);
-      // console.log(csv);
-      // window.location = url;
 
-      URL.revokeObjectURL(link.href);
+      saveAs(csv, "users_informations.csv");
     } catch (error) {
       console.error(error);
     }
