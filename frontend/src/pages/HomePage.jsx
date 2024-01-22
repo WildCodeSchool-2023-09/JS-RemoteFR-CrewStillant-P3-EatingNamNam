a1f649591b6a96a3ed91928dca30745682a65233
Mewlyscin <Tear-Lyons7@live.fr>
2024-01-22 18:02:44 +0100

    delete doublon

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -95,133 +95,125 @@ function HomePage() {
     <div className="m-20">
       <Carousel recipes={recipes} />
       <div className="border-solid border-y-4 border-orange m-10">
-        <div className="border-solid border-y-4 border-orange m-10">
-          <div className="m-10 flex flex-row justify-center text-xl gap-6 ">
-            <button
-              type="button"
-              className="bg-orange text-beige p-4 rounded-2xl border border-beige w-16"
-              onClick={() => handleAllFilters("All")}
-            >
-              All
-            </button>
-            <button
-              type="button"
-              className="bg-orange text-beige p-4 rounded-2xl border border-beige w-28"
-              onClick={() => handleAllFilters("healthy")}
-            >
-              Healthy
-            </button>
-            <button
-              type="button"
-              className="bg-orange text-beige p-4 rounded-2xl border border-beige w-28"
-              onClick={() => handleAllFilters("light")}
-            >
-              Light
-            </button>
-            <button
-              type="button"
-              className="bg-orange text-beige p-4 rounded-2xl border border-beige w-28"
-              onClick={() => handleAllFilters("fat")}
-            >
-              Fat
-            </button>
-            <div>
-              <input
-                type="input"
-                placeholder="rechercher par nom"
-                className="bg-orange text-beige p-4 rounded-2xl"
-                value={searchText}
-                onChange={handleSearch}
-              />
-            </div>
-          </div>
-          <div className="m-10 flex flex-row justify-center text-xl gap-6">
-            <label className="bg-orange text-beige p-2 rounded-2xl border border-beige">
-              Difficulté:
-              <select
-                className="bg-orange text-beige p-2 rounded-2xl border border-beige m-1"
-                onChange={difficultyFiltered}
-              >
-                <option value="">Tous</option>
-                <option value="1">facile</option>
-                <option value="2">moyen</option>
-                <option value="3">difficile</option>
-              </select>
-            </label>
-            <label className="bg-orange text-beige p-2 rounded-2xl border border-beige">
-              Temps:
-              <select
-                className="bg-orange text-beige p-2 rounded-2xl border border-beige m-1"
-                onChange={handleTimeFilter}
-              >
-                <option value="">Tous</option>
-                <option value="10-20">10-20min</option>
-                <option value="20-30">20-30min</option>
-                <option value="35+">plus de 35min</option>
-              </select>
-            </label>
+        <div className="m-10 flex flex-row justify-center text-xl gap-6 ">
+          <button
+            type="button"
+            className="bg-orange text-beige p-4 rounded-2xl border border-beige w-16"
+            onClick={() => handleAllFilters("All")}
+          >
+            All
+          </button>
+          <button
+            type="button"
+            className="bg-orange text-beige p-4 rounded-2xl border border-beige w-28"
+            onClick={() => handleAllFilters("healthy")}
+          >
+            Healthy
+          </button>
+          <button
+            type="button"
+            className="bg-orange text-beige p-4 rounded-2xl border border-beige w-28"
+            onClick={() => handleAllFilters("light")}
+          >
+            Light
+          </button>
+          <button
+            type="button"
+            className="bg-orange text-beige p-4 rounded-2xl border border-beige w-28"
+            onClick={() => handleAllFilters("fat")}
+          >
+            Fat
+          </button>
+          <div>
+            <input
+              type="input"
+              placeholder="rechercher par nom"
+              className="bg-orange text-beige p-4 rounded-2xl"
+              value={searchText}
+              onChange={handleSearch}
+            />
           </div>
         </div>
-        <div>
-          {auth.token ? (
-            <h1 className="text-center text-4xl">
-              Bienvenue {auth.userVerified.pseudo} !
-            </h1>
-          ) : null}
-          <div className={!auth.token ? "blur-sm" : null}>
-            {/* // blur-sm */}
-            <ul className="flex flex-row justify-center flex-wrap gap-16">
-              <div className="rounded-2xl w-72 h-80 p-4 bg-green text-center">
-                <h1 className="text-beige text-2xl">Créer ma recette</h1>
-                <NavLink to="/new-recipe">
-                  <div className="flex flex-col items-center justify-center mt-16">
-                    <button
-                      type="button"
-                      className="rounded-full bg-beige p-10 text-green font-bold text-4xl"
-                    >
-                      +
-                    </button>
+        <div className="m-10 flex flex-row justify-center text-xl gap-6">
+          <label className="bg-orange text-beige p-2 rounded-2xl border border-beige">
+            Difficulté:
+            <select
+              className="bg-orange text-beige p-2 rounded-2xl border border-beige m-1"
+              onChange={difficultyFiltered}
+            >
+              <option value="">Tous</option>
+              <option value="1">facile</option>
+              <option value="2">moyen</option>
+              <option value="3">difficile</option>
+            </select>
+          </label>
+          <label className="bg-orange text-beige p-2 rounded-2xl border border-beige">
+            Temps:
+            <select
+              className="bg-orange text-beige p-2 rounded-2xl border border-beige m-1"
+              onChange={handleTimeFilter}
+            >
+              <option value="">Tous</option>
+              <option value="10-20">10-20min</option>
+              <option value="20-30">20-30min</option>
+              <option value="35+">plus de 35min</option>
+            </select>
+          </label>
+        </div>
+      </div>
+      <div>
+        {auth.token ? (
+          <h1 className="text-center text-4xl">
+            Bienvenue {auth.userVerified.pseudo} !
+          </h1>
+        ) : null}
+        <div className={!auth.token ? "blur-sm" : null}>
+          {/* // blur-sm */}
+          <ul className="flex flex-row justify-center flex-wrap gap-16">
+            <div className="rounded-2xl w-72 h-80 p-4 bg-green text-center">
+              <h1 className="text-beige text-2xl">Créer ma recette</h1>
+              <NavLink to="/new-recipe">
+                <div className="flex flex-col items-center justify-center mt-16">
+                  <button
+                    type="button"
+                    className="rounded-full bg-beige p-10 text-green font-bold text-4xl"
+                  >
+                    +
+                  </button>
+                </div>
+              </NavLink>
+            </div>
+            {filteredRecipes.map((r) => (
+              <li
+                key={r.id}
+                className="rounded-2xl w-72 h-80 p-0 bg-green text-center"
+              >
+                <h1 className="text-beige text-lg m-2 uppercase">{r.title}</h1>
+                <div className="flex flex-row justify-between m-2">
+                  <div className="flex flex-row h-6 w-7">
+                    {difficultyEmoji(r.difficulty)}
                   </div>
-                </NavLink>
-              </div>
-              {filteredRecipes.map((r) => (
-                <li
-                  key={r.id}
-                  className="rounded-2xl w-72 h-80 p-0 bg-green text-center"
-                >
-                  <h1 className="text-beige text-lg m-2 uppercase">
-                    {r.title}
-                  </h1>
-                  <div className="flex flex-row justify-between m-2">
-                    <div className="flex flex-row h-6 w-7">
-                      {difficultyEmoji(r.difficulty)}
-                    </div>
-                    <p className="text-beige mb-2 flex flex-row">
-                      <img
-                        className="h-8 w-8 items-center mb-2"
-                        src={timer}
-                        alt={timer}
-                      />
-                      : {r.preparation_time} min
-                    </p>
-                  </div>
-                  <img
-                    className="rounded-2xl h-56"
-                    src={r.image}
-                    alt={r.image}
-                  />
-                  <div className="flex flex-col items-center justify-center">
-                    <NavLink
-                      to={`/recipe/${r.id}`}
-                      className="rounded-xl mb-14 bg-green text-beige p-1.5 absolute"
-                    >
-                      En savoir plus
-                    </NavLink>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </div>
+                  <p className="text-beige mb-2 flex flex-row">
+                    <img
+                      className="h-8 w-8 items-center mb-2"
+                      src={timer}
+                      alt={timer}
+                    />
+                    : {r.preparation_time} min
+                  </p>
+                </div>
+                <img className="rounded-2xl h-56" src={r.image} alt={r.image} />
+                <div className="flex flex-col items-center justify-center">
+                  <NavLink
+                    to={`/recipe/${r.id}`}
+                    className="rounded-xl mb-14 bg-green text-beige p-1.5 absolute"
+                  >
+                    En savoir plus
+                  </NavLink>
+                </div>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </div>
